11474fe85423c3e18c9b1ef4083760de5116c503
EveryThingPlay <kokochiha30@gmail.com>
2023-04-07 16:21:07 +0300

    Add CButton component

---
+++ src/components/CButton.vue
@@ -0,0 +1,55 @@
+<script setup lang="ts">
+import type { PropType } from 'vue'
+
+type variant = 'primary' | 'icon'
+
+const props = defineProps({
+  variant: { type: String as PropType<variant>, default: 'primary' },
+})
+</script>
+
+<template>
+  <div class="cbutton">
+    <button :class="`cbutton-button cbutton-${variant}`">
+      <slot />
+    </button>
+  </div>
+</template>
+
+<style>
+.cbutton-button {
+  font-size: 15px;
+  font-family: 'Rubik', sans-serif;
+  font-weight: 600;
+  color: #2D3B87;
+  background: #F4F5F9;
+  border-radius: 10px;
+  cursor: pointer;
+  outline: none;
+  border: none;
+  transition-duration: 100ms;
+}
+
+.cbutton-button:hover {
+  color: #2D3B87;
+  background: #bfc1c9;
+}
+
+.cbutton-button:active {
+  color: #2D3B87;
+  background: #9a9dad;
+}
+
+.cbutton-primary {
+  padding: 1rem 2rem 1rem 2rem;
+}
+
+.cbutton-primary:active {
+  padding: 1.05rem 2rem 0.95rem 2rem;
+}
+
+.cbutton-icon {
+  padding: 1rem 1rem 1rem 1rem;
+  transition-duration: 50ms;
+}
+</style>
---
+++ src/views/ComponentsView.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import CButton from '@/components/CButton.vue'
+
+function clicked() {
+  alert('sasa')
+}
+</script>
+
+<template>
+  <div style="display: flex; flex-direction: column; gap: 1rem; padding: 2rem">
+    <div style="display: flex; flex-direction: column; gap: 0.5rem">
+      <h2>Кнопки</h2>
+      <CButton @click="clicked()">
+        Добавить в сделки
+      </CButton>
+      <CButton variant="icon">
+        <div style="height: 1rem; width: 1rem">
+          :D
+        </div>
+      </CButton>
+    </div>
+  </div>
+</template>
