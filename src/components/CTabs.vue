da9ef421f48b4669c90dd7b259649bc4b7715fb4
EveryThingPlay <kokochiha30@gmail.com>
2023-04-12 17:17:47 +0300

    Add Ctabs component + add icons + comment default dark theme

---
+++ src/components/CTabs.vue
@@ -0,0 +1,102 @@
+<script setup lang="ts">
+import type { PropType } from 'vue'
+import router from '@/router'
+
+interface tab {
+  id: number | string
+  title: string
+  icon?: string
+  to?: string
+}
+
+type variant = 'text' | 'nav'
+
+defineProps({
+  tabs: { type: Array<tab> },
+  modelValue: { type: Object as PropType<tab>, required: true },
+  variant: { type: String as PropType<variant>, default: 'text' },
+})
+
+const emit = defineEmits(['update:modelValue'])
+
+function selectTab(tab: tab) {
+  emit('update:modelValue', tab)
+  if (tab.to)
+    router.push(tab.to)
+}
+
+function icon(val: string) {
+  return new URL(`../assets/icons/${val}`, import.meta.url).toString()
+}
+</script>
+
+<template>
+  <div v-if="variant === 'text'" class="tabs-container">
+    <div
+      v-for="val in tabs" :key="val.id" class="tab"
+      :class="modelValue.id === val.id ? 'active' : null" @click="selectTab(val)"
+    >
+      {{ val.title }}
+    </div>
+  </div>
+  <div v-if="variant === 'nav'" class="nav-container">
+    <div
+      v-for="val in tabs" :key="val.id" class="tab"
+      :class="modelValue.id === val.id ? 'active' : null" @click="selectTab(val)"
+    >
+      <img v-if="val.icon" :src="icon(val.icon)" width="20">
+      {{ val.title }}
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.tabs-container {
+  display: flex;
+  gap: 8px;
+  height: 40px;
+  flex-direction: row;
+  background: #F4F5F9;
+  align-items: center;
+  padding: 0 12px 0 12px;
+  border-radius: 10px;
+  border: solid 1px #E0E3EE;
+}
+.tabs-container > .tab {
+  color: #969DC3;
+  cursor: pointer;
+  user-select: none;
+}
+
+.tabs-container > .active {
+  color: #2D3B87;
+}
+
+.nav-container {
+  display: flex;
+  gap: 8px;
+  flex-direction: row;
+  align-items: center;
+}
+.nav-container > .tab {
+  min-width: 86px;
+  height: 50px;
+  color: #2D3B87;
+  cursor: pointer;
+  user-select: none;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 4px;
+  padding: 7px 7px 7px 7px;
+  font-weight: 500;
+  font-size: 13px;
+  line-height: 100%;
+}
+
+.nav-container > .active {
+  color: #2D3B87;
+  background: #F4F5F9;
+  border-radius: 10px;
+}
+</style>
--- src/views/ComponentsView.vue
+++ src/views/ComponentsView.vue
@@ -1,16 +1,34 @@
 <script setup lang="ts">
+import { ref } from 'vue'
 import CButton from '@/components/CButton.vue'
+import CTabs from '@/components/CTabs.vue'
 
-function clicked() {
-  alert('sasa')
-}
+// Для табов
+const tabs = [
+  {
+    id: 1,
+    title: 'Избранное',
+    icon: 'heart.svg',
+  },
+  {
+    id: 2,
+    title: 'Склад',
+    icon: 'box.svg',
+  },
+  {
+    id: 3,
+    title: 'Сделки',
+    icon: 'bag.svg',
+  },
+]
+const tab = ref(tabs[0])
 </script>
 
 <template>
   <div style="display: flex; flex-direction: column; gap: 1rem; padding: 2rem">
-    <div style="display: flex; flex-direction: column; gap: 0.5rem">
+    <div class="component">
       <h2>Кнопки</h2>
-      <CButton @click="clicked()">
+      <CButton>
         Добавить в сделки
       </CButton>
       <CButton variant="icon">
@@ -19,5 +37,20 @@ function clicked() {
         </div>
       </CButton>
     </div>
+    <div class="component">
+      <h2>Табы</h2>
+      <CTabs v-model="tab" :tabs="tabs" />
+      <CTabs v-model="tab" :tabs="tabs" variant="nav" />
+      {{ tab }}
+    </div>
   </div>
 </template>
+
+<style scoped>
+.component {
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
+  align-items: flex-start;
+}
+</style>
