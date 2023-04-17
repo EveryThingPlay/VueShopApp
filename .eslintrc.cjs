4e732883d183226a981ead2ce15f1142bda0a81d
EveryThingPlay <kokochiha30@gmail.com>
2023-04-17 09:45:18 +0300

    CSearch + CButton fixes

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,10 +4,10 @@ require('@rushstack/eslint-patch/modern-module-resolution')
 module.exports = {
   root: true,
   extends: [
-    '@antfu',
     'plugin:vue/vue3-essential',
     'eslint:recommended',
     '@vue/eslint-config-typescript',
+    '@antfu',
   ],
   parserOptions: {
     ecmaVersion: 'latest',
